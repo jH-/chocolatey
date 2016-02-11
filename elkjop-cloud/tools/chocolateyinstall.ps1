4ff1995da68d7e9e05c061f70b19cfc719a037f5
jH- <jojohaug@gmail.com>
2016-02-11 11:48:47 +0100

    Initial commit

---
+++ elkjop-cloud/tools/chocolateyinstall.ps1
@@ -0,0 +1,38 @@
+﻿
+$ErrorActionPreference = 'Stop';
+
+
+$packageName= 'elkjop-cloud'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://www.jottacloud.com/update/elkjop/release/setup.exe'
+$url64      = ''
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+
+  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+
+  softwareName  = 'Elkjøp Cloud*'
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
